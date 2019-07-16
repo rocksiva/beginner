237d356f15f30bb07e2168f83891c86c4cad9bf2
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-16 10:25:07 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,11 +1,11 @@
-int=(input(a))
-if(a%100==0):
-  if(a%400==0):
-    print(yes)
-  else:
-    print(no)
+a=int(input())
+if (a%4==0):
+	if (a%100==0):
+		if (a%400==0):
+			print ("yes")
+		else:
+			print ("no")
+	else:
+		print ("yes")
 else:
-  if(a%4==0):
-    print(yes)
-  else:
-    print(no)
+	print("no")
