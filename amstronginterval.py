296a55c59f9147f772568781b139dc48c4ea6306
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-07-17 09:32:44 +0530

    Create amstronginterval.py

---
+++ amstronginterval.py
@@ -0,0 +1,13 @@
+a1,b1=map(int,input().split())
+for num1 in range(a1,b1 + 1):
+   sum = 0
+ 
+   
+   temp = num1
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+ 
+   if num1 == sum:
+       print(num1)
